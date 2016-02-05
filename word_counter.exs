0bbc820febe5066168cc60b126bce4bd2f040beb
younes <magrigano@oultook.fr>
2016-02-05 17:17:57 +0100

    finishing word_counter script

--- word_counter.exs
+++ word_counter.exs
@@ -6,7 +6,10 @@ defmodule Cmd do
 		options = parser(args)
 		Logger.info "#{options[:file]}"
 		word_frequecy_map = File.stream!(options[:file]) |>
-
+							word_list_stream |>
+							count_words
+		file_write(word_frequecy_map)
+		Logger.info "finished ...."					
 	end
 
 	defp parser(args) do
@@ -16,10 +19,33 @@ defmodule Cmd do
 
 	defp word_list_stream(file_stream) do
 		map_func = fn (x) -> String.split(x, ~r{[^A-Za-z0-9_]}) end
-		filter_func = fn (x) -> String.length() > 0 end
+		filter_func = fn (x) -> String.length(x) > 0 end
 		file_stream |>
 		Stream.flat_map(map_func) |>
 		Stream.filter(filter_func)
+	end
+
+	defp count_words(stream) do
+		count_words(Enum.to_list(stream), Map.new)
+	end
+	defp count_words([], map), do: map
+	defp count_words([word| rest], map) do
+		case Map.has_key?(map, word)do
+			true ->
+				map1 = Map.update!(map, word, &(&1+1))
+				count_words(rest, map1)
+			false ->
+				map1 = Map.put_new(map, word, 1)
+				count_words(rest, map1)	
+		end
+	end
+
+	defp file_write(map) do
+		reduce_func = fn (key, acc) ->
+			acc <> "#{key} : #{Map.get(map, key)} time(s)\n"
+		end 
+		output = map |> Map.keys |> Enum.reduce("", reduce_func)
+		File.write!("word_frequecy.txt", output)
 	end
 end
 
